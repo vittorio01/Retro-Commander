5363b2f1490c0b39f4d7a55c941e11f6de5d32c9
vittorio01 <vittoriopolci@live.com>
2023-05-07 13:42:53 +0200

    deploy dell'app

---
+++ src/main/java/retrocommander/Main.java
@@ -0,0 +1,9 @@
+package retrocommander;
+
+import retrocommander.retrocommander.Application;
+
+public class Main {
+    public static void main(String[] args) {
+        Application.main(args);
+    }
+}
